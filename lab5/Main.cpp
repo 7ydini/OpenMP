5c829f51c82fefabb071237c3c1bcb272e651dec
7ydini <61342487+7ydini@users.noreply.github.com>
2020-11-17 20:27:14 +0300

    Update Main.cpp

--- lab5/Main.cpp
+++ lab5/Main.cpp
@@ -2,157 +2,204 @@
 #include <string>
 #include <iostream>
 #include <ctime>
-#include<algorithm> 
 using namespace std;
-int main()
+void parall(size_t* a, size_t* b, size_t n);
+void noparall(size_t* a, size_t* b, size_t n);
+//void crit(size_t* a, size_t* b, size_t n);
+size_t noparallx(size_t* a, size_t* b, size_t n, size_t k, size_t j);
+void main()
 {
 	setlocale(LC_ALL, "Russian");
 	srand(time(NULL));
-	int i, N;
-	cout << "���-�� ��������� �������� A � B >>>";
+	int i;	long N;
+	cout << "Кол-во элементов массивов A и B >>>";
 	cin >> N;
-	int* a = new int[N];//���������� ������������� �������
-	int* b = new int[N];
 
-	for (int i = 0; i < N; i++)//���������� ���������� �������
+	size_t* a = new size_t[N];//объявление динамического массива
+	for (i = 0; i < N; i++)//Заполнение случайными числами
 	{
 		a[i] = rand() % 1000;
 	}
 
-	for (int i = 0; i < N; i++)
+	size_t* b = new size_t[N];//объявление динамического массива
+	for (i = 0; i < N; i++)//Заполнение случайными числами
 	{
 		b[i] = rand() % 1000;
 	}
-	
-	double time = omp_get_wtime();
-	int total = 0, sum = 0;
-#pragma omp parallel sections private(sum, i)
+
+
+	size_t th_num, num_ths, max_th;
+	max_th = omp_get_max_threads();//Максимальное кол-во потоков
+	printf_s("Max threads= %zu\n", max_th);
+#pragma omp parallel num_threads(max_th) private (num_ths, th_num)//проверяем использование всех потоков
 	{
-#pragma omp section		//1 section	
+		th_num = omp_get_thread_num();
+		num_ths = omp_get_num_threads();
+		printf("I am ready %zu from %zu \n", th_num, num_ths);
+	}
+
+
+	//With OpenMP
+	double time = omp_get_wtime();
+	parall(a, b, N);
+	double end = omp_get_wtime();
+	cout << "With OMP(Atomic):Время >> " << omp_get_wtime() - time << endl;
+
+	/*With OpenMP critical
+	double time2 = omp_get_wtime();
+	crit(a, b, N);
+	double end2 = omp_get_wtime();
+	cout << "With OMP(Critical):Время >> " << omp_get_wtime() - time2 << endl;*/
+
+	//Without OpenMP atomic
+	double time3 = omp_get_wtime();
+	noparall(a, b, N);
+	double end3 = omp_get_wtime();
+	cout << "Without OMP:Время >> " << omp_get_wtime() - time3 << endl;
+
+
+	double time4 = omp_get_wtime();
+size_t total = 0;
+#pragma omp parallel sections
+	{
+#pragma omp section//1 section
 		{
-			for (i = 0; i < N/8; i++)
+			total += noparallx(a, b, N, 1, 8);//j - на сколько частей разбить массив k-атая часть массива
+		}
+#pragma omp section//2 section
+		{
+			total += noparallx(a, b, N, 2, 8);
+		}
+#pragma omp section//3 section
+		{
+			total += noparallx(a, b, N, 3, 8);
+		}
+#pragma omp section//4 section
+		{
+			total += noparallx(a, b, N, 4, 8);
+		}
+#pragma omp section//5 section
+		{
+			total += noparallx(a, b, N, 5, 8);
+		}
+#pragma omp section//6 section
+		{
+			total += noparallx(a, b, N, 6, 8);
+		}
+#pragma omp section//7 section
+		{
+			total += noparallx(a, b, N, 7, 8);
+		}
+#pragma omp section//8 section
+		{
+			total += noparallx(a, b, N, 8, 8);
+		}
+	}
+	cout << "8 sections >> Сумма значений MAX(A[i] + B[i],4*A[i] - B[i]) равна>> " << total << endl;
+	double end4 = omp_get_wtime();
+	cout << "With OMP:Время >> " << end4 - time4 << endl;
+
+
+
+	double time5 = omp_get_wtime();
+	total = 0;
+#pragma omp parallel sections
+	{
+#pragma omp section//1 section
+		{
+			total += noparallx(a, b, N, 1, 4);//j - на сколько частей разбить массив k-атая часть массива n1 - 1 если первый, 0 в ост случаях
+		}
+#pragma omp section//2 section
+		{
+			total += noparallx(a, b, N, 2, 4);
+		}
+#pragma omp section//3 section
+		{
+			total += noparallx(a, b, N, 3, 4);
+		}
+#pragma omp section//4 section
+		{
+			total += noparallx(a, b, N, 4, 4);
+		}
+	}
+	cout << "4 sections >> Сумма значений MAX(A[i] + B[i],4*A[i] - B[i]) равна>> " << total << endl;
+	double end5 = omp_get_wtime();
+	cout << "With OMP:Время >> " << end5 - time5 << endl;
+
+
+}
+
+void noparall(size_t* a, size_t* b, size_t n)
+{
+	int sum = 0, i = 0;	size_t total = 0;
+	{
+		for (i = 0; i < n; i++)
+		{
+			sum = max(a[i] + b[i], 4 * a[i] - b[i]);
+			if (sum > 1)
 			{
-				sum = a[i] + b[i];
-				if (sum > (4 * a[i] - b[i]) && sum > 1)
-				{
-					total += sum;
-				}
-				else
-				{
-					total += 4 * a[i] - b[i];
-				}
+				total += sum;
 			}
 		}
-#pragma omp section		//2 section	
+	}
+	cout << "No Parallel >> Сумма значений MAX(A[i] + B[i],4*A[i] - B[i]) равна>>" << total << "\n";
+}
+
+
+void parall(size_t* a, size_t* b, size_t n)
+{
+	int sum = 0, i = 0; size_t total = 0;
+#pragma omp parallel shared(a, b, n) private(sum, i) reduction(+:total) //Элементы массива a и b являются глобальными переменными, а для переменных i, sum - мы создаем локальные копии для работы с ними,
+	{
+#pragma omp for
+		for (i = 0; i < n; i++)
 		{
-			for (i = N/8 + 1; i < N/4; i++)
+			sum = max(a[i] + b[i], 4 * a[i] - b[i]);
+			if (sum > 1)
 			{
-				sum = a[i] + b[i];
-				if (sum > (4 * a[i] - b[i]) && sum > 1)
-				{
-					total += sum;
-				}
-				else
-				{
-					total += 4 * a[i] - b[i];
-				}
+#pragma omp atomic
+				total += sum;
 			}
 		}
-#pragma omp section		//3 section	
+	}
+	cout << "Atomic Parallel >> Сумма значений MAX(A[i] + B[i],4*A[i] - B[i]) равна>>" << total << "\n";
+}
+
+
+
+size_t noparallx(size_t* a, size_t* b, size_t n, size_t k, size_t j)//j - на сколько частей разбить массив, k-атая часть
+{
+	int sum = 0, i = 0; size_t total = 0;
+	for (i = (k - 1) * n / j; i < k * n / j; i++)//Разбили массив на j равных частей, и считаем каждую k-ю часть и складываем их
+	{
+		sum = max(a[i] + b[i], 4 * a[i] - b[i]);
+		if (sum > 1)
 		{
-			for (i = N/4+1; i < 3*N/8; i++)
-			{
-				sum = a[i] + b[i];
-				if (sum > (4 * a[i] - b[i]) && sum > 1)
-				{
-					total += sum;
-				}
-				else
-				{
-					total += 4 * a[i] - b[i];
-				}
-			}
+			total += sum;
 		}
-#pragma omp section		//4 section	
+	}
+	return total;//Возращаем тотал, для дальнейших вычислений
+}
+
+/*
+void crit(size_t* a, size_t* b, size_t n)
+{
+	int sum = 0, i = 0; size_t total = 0;
+#pragma omp parallel shared(a, b, n) private(sum, i) reduction(+:total) //Элементы массива a и b являются глобальными переменными, а для переменных i, sum - мы создаем локальные копии для работы с ними,
+	{
+#pragma omp for
+		for (i = 0; i < n; i++)
 		{
-			for (i = 3*N/8+1; i < N/2; i++)
+			sum = max(a[i] + b[i], 4 * a[i] - b[i]);
+			if (sum > 1)
 			{
-				sum = a[i] + b[i];
-				if (sum > (4 * a[i] - b[i]) && sum > 1)
+#pragma omp critical
 				{
 					total += sum;
-				}
-				else
-				{
-					total += 4 * a[i] - b[i];
-				}
-			}
-		}
-#pragma omp section		//5 section
-		{
-			for (i = N/2+1; i < 5*N/8; i++)
-			{
-				sum = a[i] - b[i];
-				if (sum > (4 * a[i] - b[i]) && sum > 1)
-				{
-					total += sum;
-				}
-				else
-				{
-					total += 4 * a[i] - b[i];
-				}
-			}
-		}
-#pragma omp section		//6 section	
-		{
-			for (i = 5*N/8 +1; i < 3*N/4; i++)
-			{
-				sum = a[i] + b[i];
-				if (sum > (4 * a[i] - b[i]) && sum > 1)
-				{
-					total += sum;
-				}
-				else
-				{
-					total += 4 * a[i] - b[i];
-				}
-			}
-		}
-#pragma omp section		//7 section	
-		{
-			for (i = 3 * N / 4 + 1; i < 7 * N / 8; i++)
-			{
-				sum = a[i] + b[i];
-				if (sum > (4 * a[i] - b[i]) && sum > 1)
-				{
-					total += sum;
-				}
-				else
-				{
-					total += 4 * a[i] - b[i];
-				}
-			}
-		}
-#pragma omp section		//8 section	
-		{
-			for (i = 7 * N / 8 + 1; i <  N; i++)
-			{
-				sum = a[i] + b[i];
-				if (sum > (4 * a[i] - b[i]) && sum > 1)
-				{
-					total += sum;
-				}
-				else
-				{
-					total += 4 * a[i] - b[i];
 				}
 			}
 		}
 	}
-	cout << "����� �������� MAX(A[i] + B[i],4*A[i] - B[i]) �����>>" << total << "\n";
-	double end = omp_get_wtime();
-	cout << omp_get_wtime() - time << endl;
-}
-//��� �������� �������� �� 10 000 000 ��������� 
-//��������� 4 ������ ������� 0.017, � ��� 0.0.
-//��������� 8 ������ ������� 0.016, � ��� 0.032.
+	cout << "Critical Parallel >> Сумма значений MAX(A[i] + B[i],4*A[i] - B[i]) равна>>" << total << "\n";
+}*/
+//При создании массивов на 10 000 000 элементов 
