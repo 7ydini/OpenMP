e0120c1031029a95f00bfb371f4bdb67c7625d65
7ydini <61342487+7ydini@users.noreply.github.com>
2020-11-17 17:42:10 +0300

    Add files via upload

--- lab3/Main.cpp
+++ lab3/Main.cpp
@@ -3,54 +3,51 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
-
-int main()
+void main()
 {
 	setlocale(LC_ALL, "Russian");
 	srand(time(NULL));
-	int i;
-	int N;
+	int i, N;
 	cout << "Кол-во элементов массивов A и B >>>";
 	cin >> N;
-	int* a = new int[N];//объявление динамического массива
-	for (int i = 0; i < N; i++)//Заполнение случайными числами
+	size_t* a = new size_t[N];//объявление динамического массива
+	for (i = 0; i < N; i++)//Заполнение случайными числами
 	{
 		a[i] = rand() % 1000;
 	}
-	int* b = new int[N];
-
-	for (int i = 0; i < N; i++)
+	size_t* b = new size_t[N];//объявление динамического массива
+	for (i = 0; i < N; i++)//Заполнение случайными числами
 	{
 		b[i] = rand() % 1000;
 	}
-	double time = omp_get_wtime();
-	int total = 0, sum = 0, s;
-#pragma omp parallel shared(a, b) private(sum, i) reduction(+:total)
-	{  //Элементы массива a и b являются глобальными переменными, а для переменных i, sum - мы создаем локальные копии для работы с ними,
-												  //Так как при парралельном использовании этих переменных произойдёт путаница.
-#pragma omp for
-		for (i = 0; i < N; i++)
-		{
-			sum = a[i] + b[i];
-			if (sum > (4 * a[i] - b[i]) && sum > 1)
-			{
-				total += sum;
-			}
-		}
-#pragma omp for
-		for (i = 0; i < N; i++)
-		{
-			sum = 4 * a[i] - b[i];
-			if (sum > (a[i] - b[i]) && sum > 1)
-			{
-				total += sum;
-			}
-		}
 
+	size_t th_num, num_ths, max_th;
+	max_th = omp_get_max_threads();//Максимальное кол-во потоков
+	printf_s("Max threads= %zu\n", max_th);
+#pragma omp parallel num_threads(max_th) private (num_ths, th_num)//проверяем использование всех потоков
+	{
+		th_num = omp_get_thread_num();
+		num_ths = omp_get_num_threads();
+		printf("I am ready %zu from %zu \n", th_num, num_ths);
+	}
+
+	double time = omp_get_wtime();
+	int total = 0, sum = 0;
+#pragma omp parallel shared(a, b, N) private(sum, i) reduction(+:total) //Элементы массива a и b являются глобальными переменными, а для переменных i, sum - мы создаем локальные копии для работы с ними,
+	{  
+#pragma omp for
+		for (i = 0; i < N; i++)
+		{
+			sum = max(a[i] + b[i], 4*a[i] - b[i]);
+			if (sum > 1)
+			{
+				total += sum;
+			}
+		}
 	} /* Завершение параллельного фрагмента */
 	cout << "Сумма значений MAX(A[i] + B[i],4*A[i] - B[i]) равна>>" << total << "\n";
 	double end = omp_get_wtime();
 	cout << omp_get_wtime() - time << endl;
 }
 //При создании массивов на 10 000 000 элементов 
-//Используя распараллеливание выходит 0.015, а без 0.05, что в 3-4 раза медленнее.
+//Используя распараллеливание выходит 0.007, а без 0.012, что в 5-6 раз медленнее.
